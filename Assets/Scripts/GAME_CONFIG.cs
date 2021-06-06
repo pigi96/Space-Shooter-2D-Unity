d36d2fa558b138c4edf02224141fa3dac8dd3514
pigi96 <janlovsin@gmail.com>
2021-06-06 13:59:22 +0200

    Runtime baking and whole basic map generation including obstacles

---
+++ Assets/Scripts/GAME_CONFIG.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+public static class GAME_CONFIG
+{
+    // Configuration for level RENDERING
+    public const int LEVEL_BOUNDS_THICKNESS = 3; // default == 3
+    public const int DEFAULT_WIDTH = 300; // default == 300
+    public const int DEFAULT_HEIGHT = 300; // default == 300
+    public const int RENDER_OUTSIDE_BOUNDS = 222; // default == 222
+    public const int STARS_DISTANCE = 30; // default == 30
+    public const int STARS_DISTANCE_INTERVAL = 9; // default == 9
+    public const int STAR_CHANCE = 3; // default == 3
+    public const int AMOUNT_OF_STAR_SIZES = 30; // default == 30
+    public const float STAR_START_SIZE = 0.0006f; // default == 0.0006f
+    public const float STAR_EXTEND_SIZE = 0.00005f; // default == 0.00005f
+    public const int OBSTACLE_DISTANCE = 60; // default == 60
+    public const int OBSTACLE_CHANCE = 2; // default == 2
+    public const int OBSTACLE_DISTANCE_INTERVAL = 9; // default == 9
+}
--- Assets/Scripts/GeneratePlayfield.cs
+++ Assets/Scripts/GeneratePlayfield.cs
@@ -1,43 +1,116 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GeneratePlayfield : MonoBehaviour
 {
-    float width = 1000;
-    float height = 1000;
-
     public GameObject playfieldObject;
-    public GameObject starPefab;
+    public GameObject starPefab, boundsPrefab, circleObstaclePrefab, squareObstaclePrefab, triangleObstaclePrefab;
 
     // Start is called before the first frame update
     void Start()
-    {
-        playfieldObject.GetComponent<Transform>().localScale = new Vector3(width, height, 0);
+    {   
+        playfieldObject.GetComponent<Transform>().localScale = new Vector3(GAME_CONFIG.DEFAULT_WIDTH + GAME_CONFIG.RENDER_OUTSIDE_BOUNDS, GAME_CONFIG.DEFAULT_HEIGHT + GAME_CONFIG.RENDER_OUTSIDE_BOUNDS, 0);
+        CreateBackgroundBounds();
         CreateBackgroundStars();
+        CreateGameObstacles();
+        gameObject.GetComponentInChildren<NavMeshSurface2d>().BuildNavMesh();
     }
 
+    void CreateBackgroundBounds()
+    {
+        GameObject leftWall = CreateBoundsWall(GAME_CONFIG.LEVEL_BOUNDS_THICKNESS, GAME_CONFIG.DEFAULT_HEIGHT + GAME_CONFIG.LEVEL_BOUNDS_THICKNESS, -GAME_CONFIG.DEFAULT_WIDTH/2, 0);
+        GameObject upWall = CreateBoundsWall(GAME_CONFIG.DEFAULT_WIDTH +GAME_CONFIG.LEVEL_BOUNDS_THICKNESS, GAME_CONFIG.LEVEL_BOUNDS_THICKNESS, 0, GAME_CONFIG.DEFAULT_HEIGHT/2);
+        GameObject rightWall = CreateBoundsWall(GAME_CONFIG.LEVEL_BOUNDS_THICKNESS, GAME_CONFIG.DEFAULT_HEIGHT+GAME_CONFIG.LEVEL_BOUNDS_THICKNESS, GAME_CONFIG.DEFAULT_WIDTH/2, 0);
+        GameObject downWall = CreateBoundsWall(GAME_CONFIG.DEFAULT_WIDTH+GAME_CONFIG.LEVEL_BOUNDS_THICKNESS, GAME_CONFIG.LEVEL_BOUNDS_THICKNESS, 0, -GAME_CONFIG.DEFAULT_HEIGHT/2);
+    }
 
+    GameObject CreateBoundsWall(float width, float height, float x, float y)
+    {
+        GameObject wall = Instantiate(boundsPrefab);
+        wall.transform.localScale = new Vector3(width, height, 0);
+        wall.transform.position = new Vector3(x, y, -2);
+        return wall;
+    }
+
+    void CreateGameObstacles()
+    {
+        for (int i = 0; i < GAME_CONFIG.DEFAULT_WIDTH; i += GAME_CONFIG.OBSTACLE_DISTANCE)
+        {
+            for (int j = 0; j < GAME_CONFIG.DEFAULT_HEIGHT; j += GAME_CONFIG.OBSTACLE_DISTANCE)
+            {
+                bool skip = Random.Range(0, GAME_CONFIG.OBSTACLE_CHANCE) == 0 ? false : true;
+                if (skip) continue;
+
+                float x = -(GAME_CONFIG.DEFAULT_WIDTH / 2) + i + Random.Range(0, GAME_CONFIG.OBSTACLE_DISTANCE_INTERVAL);
+                float y = -(GAME_CONFIG.DEFAULT_HEIGHT / 2) + j + Random.Range(0, GAME_CONFIG.OBSTACLE_DISTANCE_INTERVAL);
+
+                CreateObstacle(x, y);
+            }
+        }
+    }
+
+    void CreateObstacle(float x, float y)
+    {
+        int select = Random.Range(0, 3);
+        switch (select)
+        {
+            case 0:
+                CreateACircle(x, y);
+                break;
+            case 1:
+                CreateASquare(x, y);
+                break;
+            case 2:
+                CreateATriangle(x, y);
+                break;
+        }
+    }
+
+    void CreateACircle(float x, float y)
+    {
+        // Simple circles of different sizes
+        GameObject newObstacle = Instantiate(circleObstaclePrefab);
+        newObstacle.transform.parent = gameObject.transform;
+        newObstacle.transform.position = new Vector3(x, y, -2);
+    }
+
+    void CreateASquare(float x, float y)
+    {
+        // Randomly combine multiple squares
+        GameObject newObstacle = Instantiate(squareObstaclePrefab);
+        newObstacle.transform.parent = gameObject.transform;
+        newObstacle.transform.position = new Vector3(x, y, -2);
+    }
+
+    void CreateATriangle(float x, float y)
+    {
+        // Randomly combine multiple triangles
+        GameObject newObstacle = Instantiate(triangleObstaclePrefab);
+        newObstacle.transform.parent = gameObject.transform;
+        newObstacle.transform.position = new Vector3(x, y, -2);
+    }
 
     void CreateBackgroundStars()
     {
-        for (int i = 5; i < width; i += 30)
+        for (int i = 0; i < GAME_CONFIG.DEFAULT_WIDTH + GAME_CONFIG.RENDER_OUTSIDE_BOUNDS; i += GAME_CONFIG.STARS_DISTANCE)
         {
-            for (int j = 5; j < height; j += 30)
+            for (int j = 0; j < GAME_CONFIG.DEFAULT_HEIGHT + GAME_CONFIG.RENDER_OUTSIDE_BOUNDS; j += GAME_CONFIG.STARS_DISTANCE)
             {
-                bool skip = Random.Range(0, 3) == 0 ? false : true;
+                bool skip = Random.Range(0, GAME_CONFIG.STAR_CHANCE) == 0 ? false : true;
                 if (skip) continue;
 
                 GameObject newStar = Instantiate(starPefab);
                 newStar.transform.parent = playfieldObject.transform;
 
-                float x = -width / 2 + i + Random.Range(0, 9);
-                float y = -height / 2 + j + Random.Range(0, 9);
+                float x = -(GAME_CONFIG.DEFAULT_WIDTH / 2 + GAME_CONFIG.RENDER_OUTSIDE_BOUNDS/2 ) + i + Random.Range(0, GAME_CONFIG.STARS_DISTANCE_INTERVAL);
+                float y = -(GAME_CONFIG.DEFAULT_HEIGHT / 2 + GAME_CONFIG.RENDER_OUTSIDE_BOUNDS / 2) + j + Random.Range(0, GAME_CONFIG.STARS_DISTANCE_INTERVAL);
 
                 newStar.transform.position = new Vector3(x, y, 1);
 
-                float size = Random.Range(0, 30);
-                newStar.transform.localScale = new Vector3(0.0006f + (0.00005f*size), 0.0006f + (0.00005f*size), 0);
+                float size = Random.Range(0, GAME_CONFIG.AMOUNT_OF_STAR_SIZES);
+                newStar.transform.localScale = new Vector3(GAME_CONFIG.STAR_START_SIZE + (GAME_CONFIG.STAR_EXTEND_SIZE*size), GAME_CONFIG.STAR_START_SIZE + (GAME_CONFIG.STAR_EXTEND_SIZE * size), 0);
             }
         }
     }
